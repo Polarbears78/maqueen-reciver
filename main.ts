23f6343e4ccbea6be7a6a04f11f1ca18ba12d047
Polarbears78 <91547292+Polarbears78@users.noreply.github.com>
2022-04-24 11:39:54 +0900

    MakeCode 프로젝트용 기초 파일

---
+++ main.ts
@@ -0,0 +1,28 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    if (receivedNumber == 20) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    }
+    if (receivedNumber == 10) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    }
+    if (receivedNumber == 40) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+    }
+    if (receivedNumber == 30) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 50)
+    }
+    if (receivedNumber == 50) {
+        maqueen.motorStop(maqueen.Motors.All)
+    }
+})
+radio.setGroup(75)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    # . # . #
+    . . . . .
+    . . . . .
+    `)
+basic.forever(function () {
+	
+})
